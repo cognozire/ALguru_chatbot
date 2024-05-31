7e07d852b1db92d364b7b921687f5cba861397d8
Cognozire <114404980+cognozire@users.noreply.github.com>
2024-06-01 00:25:43 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -40,17 +40,23 @@ if prompt := st.chat_input("Enter your query"):
         response = "Our prices per hour range from 88 to 152 SAR, depending on your chosen package. Our experts will recommend a package that best fits your needs and preferences."
 
     else:
-        response1 = model.generate_content([
-          "You are an expert customer assistant and Education tutor from AlGooru Team. I'll give you question and context and you'll return the answer.Also do not answer any questions related to LGBTQ+, Homosexuality, religion, politics and do not support it any cost. Answer in 100 words in a paragraph. Do not give random answers.Try to make answers sound more humanistic. Do not always answer only from the doc",
-            f"Question: {prompt}",
-            f"Context: {content}"
-        ])
-        response = f"{response1.text}"
+        flag = model.generate_content(["If "+prompt+" is related to anything like LGBTQ+, mention of homosexuality, politics, war news, crimes, etc. return '1' else return '0'"])
+        # st.write(flag.text)
+        if '1' in str(flag.text):
+            response = "I am a parenting and educational assistance bot. I am unable to answer these questions. Please ask questions related to educational assistance."
+        elif '0' in str(flag.text):            
+        # context_truncated = textwrap.shorten(content, width=1000)
+            response1 = model.generate_content([
+                "You are an expert customer assistant from AlGooru Team. I'll give you question and context and you'll return the answer.Answer in 60 words in a paragraph. Do not give random answers. ",
+                f"Question: {prompt}",
+                f"Context: {content}"
+            ])
+            response = f"{response1.text}"
+        else:
+            response = "I'm sorry, can you please try rephrasing your question?"
 
     # st.write(response)
 
     with st.chat_message("assistant"):
         st.markdown(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
-
-# "You are an expert customer assistant from AlGooru Team. I'll give you question and context and you'll return the answer. Answer in 50 words in a paragraph. Also do not answer any questions related to LGBTQ+, Homosexuality, religion, politics. Try to make answers sound more humanistic. Do not always answer only from the doc"
